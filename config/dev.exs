71db0d35dded632376d766f73643f14f8a1f25fa
scottming <therealscottming@gmail.com>
2019-02-02 09:46:28 +0800

    Fix the config

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :plate_slate, PlateSlate.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "scottming",
+  password: "",
   database: "plate_slate_dev",
   hostname: "localhost",
   pool_size: 10
