e0fc3ccb9ae3a99ba14db9b477e782fca7cf8276
scottming <therealscottming@gmail.com>
2019-02-02 17:16:04 +0800

    Add @desc to menuItems

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,24 +1,23 @@
-#---
+# ---
 # Excerpted from "Craft GraphQL APIs in Elixir with Absinthe",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/wwgraphql for more book information.
-#---
+# ---
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
   alias PlateSlate.{Menu, Repo}
 
   query do
-
+    @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
-      resolve fn _, _, _ ->
+      resolve(fn _, _, _ ->
         {:ok, Repo.all(Menu.Item)}
-      end
+      end)
     end
-
   end
 
   object :menu_item do
@@ -26,6 +25,4 @@ defmodule PlateSlateWeb.Schema do
     field :name, :string
     field :description, :string
   end
-
-
 end
