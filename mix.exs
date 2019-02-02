db4376911f7f6947b02ecdfddd4d161e6dab8caf
scottming <therealscottming@gmail.com>
2019-02-02 17:15:32 +0800

    update absinthe to the lastest version

--- mix.exs
+++ mix.exs
@@ -43,10 +43,10 @@ defmodule PlateSlate.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe, "~> 1.4.13"},
-      {:absinthe_plug, "~> 1.4.0"},
-      {:absinthe_phoenix, "~> 1.4.0"},
-      {:absinthe_relay, "~> 1.4.0"},
+      {:absinthe, "~> 1.4.14"},
+      {:absinthe_plug, "~> 1.4.6"},
+      {:absinthe_phoenix, "~> 1.4.3"},
+      {:absinthe_relay, "~> 1.4.6"},
     ]
   end
 
