c5b19fbb8528308775f0bc1a9d9eb2bcad9c1733
scottming <therealscottming@gmail.com>
2019-02-03 22:44:41 +0800

    Add more filter

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -10,18 +10,38 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
   alias PlateSlateWeb.Resolvers
 
+  query do
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      arg :filter, :menu_item_filter
+      arg :order, type: :sort_order, default_value: :asc
+      resolve &Resolvers.Menu.menu_items/3
+    end
+  end
+
   enum :sort_order do
     value :asc
     value :desc
   end
 
-  query do
-    @desc "The list of available items on the menu"
-    field :menu_items, list_of(:menu_item) do
-      arg :matching, :string
-      arg :order, type: :sort_order, default_value: :asc
-      resolve &Resolvers.Menu.menu_items/3
-    end
+  @desc "Filtering options for the menu item list"
+  input_object :menu_item_filter do
+
+    @desc "Matching a name"
+    field :name, :string
+
+    @desc "Matching a category name"
+    field :category, :string
+
+    @desc "Matching a tag"
+    field :tag, :string
+
+    @desc "Priced above a value"
+    field :priced_above, :float
+
+    @desc "Priced below a value"
+    field :priced_below, :float
+
   end
 
   object :menu_item do
