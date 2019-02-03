b2bb774e124bc4dae93ead9456cba417f21c6b84
scottming <therealscottming@gmail.com>
2019-02-03 23:27:16 +0800

    Add date filter

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -13,20 +13,19 @@ defmodule PlateSlateWeb.Schema do
   query do
     @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
-      arg :filter, :menu_item_filter
-      arg :order, type: :sort_order, default_value: :asc
-      resolve &Resolvers.Menu.menu_items/3
+      arg(:filter, :menu_item_filter)
+      arg(:order, type: :sort_order, default_value: :asc)
+      resolve(&Resolvers.Menu.menu_items/3)
     end
   end
 
   enum :sort_order do
-    value :asc
-    value :desc
+    value(:asc)
+    value(:desc)
   end
 
   @desc "Filtering options for the menu item list"
   input_object :menu_item_filter do
-
     @desc "Matching a name"
     field :name, :string
 
@@ -42,11 +41,31 @@ defmodule PlateSlateWeb.Schema do
     @desc "Priced below a value"
     field :priced_below, :float
 
+    @desc "Added to the menu before this date"
+    field :added_before, :date
+
+    @desc "Added to the menu after this date"
+    field :added_after, :date
+
+  end
+
+  scalar :date do
+    parse(fn input ->
+      case Date.from_iso8601(input.value) do
+        {:ok, date} -> {:ok, date}
+        _ -> :error
+      end
+    end)
+
+    serialize(fn date ->
+      Date.to_iso8601(date)
+    end)
   end
 
   object :menu_item do
     field :id, :id
     field :name, :string
     field :description, :string
+    field :added_on, :date
   end
 end
