6bae0abfa9bff41408ce332c7e7845cb970d8095
scottming <therealscottming@gmail.com>
2019-02-04 23:37:46 +0800

    Format error

--- lib/plate_slate_web/schema/menu_types.ex
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -61,6 +61,11 @@ defmodule PlateSlateWeb.Schema.MenuTypes do
     field :category_id, non_null(:id)
   end
 
+  object :menu_item_result do
+    field :menu_item, :menu_item
+    field :errors, list_of(:input_error)
+  end
+
   object :menu_item do
     interfaces [:search_result]
     field :id, :id
--- test/plate_slate_web/schema/mutation/create_menu_item_test.exs
+++ test/plate_slate_web/schema/mutation/create_menu_item_test.exs
@@ -27,9 +27,12 @@ defmodule PlateSlateWeb.Schema.Mutation.CreateMenuTest do
   @query """
   mutation ($menuItem: MenuItemInput!) {
     createMenuItem(input: $menuItem) {
-      name
-      description
-      price
+      errors { key message }
+      menuItem {
+        name
+        description
+        price
+      }
     }
   }
   """
@@ -47,10 +50,38 @@ defmodule PlateSlateWeb.Schema.Mutation.CreateMenuTest do
 
     assert json_response(conn, 200) == %{
       "data" => %{
-       "createMenuItem" => %{
-          "name" => menu_item["name"],
-          "description" => menu_item["description"],
-          "price" => menu_item["price"]
+        "createMenuItem" => %{
+          "errors" => nil,
+          "menuItem" => %{
+            "name" => menu_item["name"],
+            "description" => menu_item["description"],
+            "price" => menu_item["price"]
+          }
+        }
+      }
+    }
+  end
+
+  test "creating a menu item with an existing name fails",
+  %{category_id: category_id} do
+    menu_item = %{
+      "name" => "Reuben",
+      "description" => "Roast beef, caramelized onions, horseradish, ...",
+      "price" =>  "5.75",
+      "categoryId" => category_id,
+    }
+    conn = build_conn()
+    conn = post conn, "/api",
+      query: @query,
+      variables: %{"menuItem" => menu_item}
+
+    assert json_response(conn, 200) == %{
+      "data" => %{
+        "createMenuItem" => %{
+          "errors" => [
+            %{"key" => "name", "message" => "has already been taken"}
+          ],
+          "menuItem" => nil
         }
       }
     }
