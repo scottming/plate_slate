222ef2b708505b236188e40790d41a4f7b7440a4
scottming <therealscottming@gmail.com>
2019-02-05 15:47:33 +0800

    Add triger

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -33,6 +33,16 @@ defmodule PlateSlateWeb.Schema do
       resolve &Resolvers.Ordering.place_order/3
     end
 
+    field :ready_order, :order_result do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Ordering.ready_order/3
+    end
+
+    field :complete_order, :order_result do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Ordering.complete_order/3
+    end
+
     field :create_menu_item, :menu_item_result do
       arg :input, non_null(:menu_item_input)
       resolve &Resolvers.Menu.create_item/3
@@ -42,6 +52,19 @@ defmodule PlateSlateWeb.Schema do
   subscription do
     field :new_order, :order do
       config fn _args, _info -> {:ok, topic: "*"} end
+    end
+
+    field :update_order, :order do
+      arg :id, non_null(:id)
+      config fn args, _info -> {:ok, topic: args.id} end
+
+      trigger [:ready_order, :complete_order],
+        topic: fn
+          %{order: order} -> [order.id]
+          _ -> []
+        end
+
+      resolve fn %{order: order}, _, _ -> {:ok, order} end
     end
   end
 
